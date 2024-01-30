e8bd7c9d68e26087802bc56428f8881b948ae7e7
darkbit <@darkbit:stratum0.org>
2024-01-30 02:09:28 +0100

    concentratorconfig is now working

--- concentratorconfig/main.go
+++ concentratorconfig/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"sort"
 	"time"
 
 	"gitli.stratum0.org/ffbs/etcd-tools/ffbs"
@@ -14,84 +19,126 @@ import (
 
 const WG_DEVICENAME = "wg-nodes"
 
-func update(etcd *ffbs.EtcdHandler, wg *wgctrl.Client) error {
-	nodes, _, err := etcd.GetAllNodeInfo(context.Background())
+func sortIPNet(s []net.IPNet) {
+	sort.Slice(s, func(i, j int) bool {
+		res := bytes.Compare(s[i].IP[:], s[j].IP[:])
+		if res != 0 {
+			return res > 0
+		}
+		res = bytes.Compare(s[i].Mask[:], s[j].Mask[:])
+		return res > 0
+	})
+}
+
+func calculateWGPeerUpdates(etcd *ffbs.EtcdHandler, wg *wgctrl.Client) ([]wgtypes.PeerConfig, error) {
+	nodes, defNode, err := etcd.GetAllNodeInfo(context.Background())
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	dev, err := wg.Device(WG_DEVICENAME)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	var updates wgtypes.Config
-	updates.Peers = make([]wgtypes.PeerConfig, 0, 10)
+	updates := make([]wgtypes.PeerConfig, 0, 10)
 
 	// remove and update existing nodes
 	for _, peer := range dev.Peers {
-		pubkey := peer.PublicKey.String()
+		pubkey := base64.URLEncoding.EncodeToString(peer.PublicKey[:])
 		node, ok := nodes[pubkey]
 		delete(nodes, pubkey)
 		if !ok {
 			// remove key as node vanished
-			updates.Peers = append(updates.Peers, wgtypes.PeerConfig{
+			updates = append(updates, wgtypes.PeerConfig{
 				PublicKey: peer.PublicKey,
 				Remove:    true,
 			})
 			continue
 		}
 
-		// TODO catch range4/range6 being nil/not set
-		_, range4, err := net.ParseCIDR(*node.Range4)
-		if err != nil {
-			return err
-		}
-		_, range6, err := net.ParseCIDR(*node.Range6)
-		if err != nil {
-			return err
-		}
-		// TODO check for changes and skip, if nothing changed
+		nets := node.IPNets()
+		sortIPNet(nets)
+		sortIPNet(peer.AllowedIPs)
 
-		hardcodedKeepalive := 15 * time.Second
-		updates.Peers = append(updates.Peers, wgtypes.PeerConfig{
-			PublicKey:                   peer.PublicKey,
-			PersistentKeepaliveInterval: &hardcodedKeepalive,
-			ReplaceAllowedIPs:           true,
-			AllowedIPs:                  []net.IPNet{*range4, *range6},
-		})
+		equalNet := true
+		if len(nets) != len(peer.AllowedIPs) {
+			equalNet = false
+		} else {
+			for i, cur := range nets {
+				if !bytes.Equal(cur.IP[:], peer.AllowedIPs[i].IP[:]) {
+					equalNet = false
+					break
+				}
+				if !bytes.Equal(cur.Mask[:], peer.AllowedIPs[i].Mask[:]) {
+					equalNet = false
+					break
+				}
+			}
+		}
+
+		keepalive := node.WGKeepaliveTime()
+		if keepalive == nil {
+			keepalive = defNode.WGKeepaliveTime()
+			if keepalive == nil {
+				disable := 0 * time.Second
+				keepalive = &disable
+			}
+		}
+		keepaliveChanged := *keepalive != peer.PersistentKeepaliveInterval
+
+		if !equalNet || keepaliveChanged {
+			updates = append(updates, wgtypes.PeerConfig{
+				PublicKey:                   peer.PublicKey,
+				PersistentKeepaliveInterval: keepalive,
+				ReplaceAllowedIPs:           true,
+				AllowedIPs:                  nets,
+			})
+		}
 	}
 
 	// add new nodes
 	for pubkey, node := range nodes {
-		pkey, err := wgtypes.ParseKey(pubkey)
+		decpkey, err := base64.URLEncoding.DecodeString(pubkey)
 		if err != nil {
-			return err
+			return nil, fmt.Errorf("couldn't base64 decode pubkey '%s'", pubkey)
+		}
+		pkey, err := wgtypes.NewKey(decpkey)
+		if err != nil {
+			return nil, err
 		}
 
-		// TODO catch range4/range6 being nil/not set
-		_, range4, err := net.ParseCIDR(*node.Range4)
-		if err != nil {
-			return err
-		}
-		_, range6, err := net.ParseCIDR(*node.Range6)
-		if err != nil {
-			return err
+		keepalive := node.WGKeepaliveTime()
+		if keepalive == nil {
+			keepalive = defNode.WGKeepaliveTime()
+			if keepalive == nil {
+				disable := 0 * time.Second
+				keepalive = &disable
+			}
 		}
 
-		hardcodedKeepalive := 15 * time.Second
-		updates.Peers = append(updates.Peers, wgtypes.PeerConfig{
+		updates = append(updates, wgtypes.PeerConfig{
 			PublicKey:                   pkey,
-			PersistentKeepaliveInterval: &hardcodedKeepalive,
+			PersistentKeepaliveInterval: keepalive,
 			ReplaceAllowedIPs:           true,
-			AllowedIPs:                  []net.IPNet{*range4, *range6},
+			AllowedIPs:                  node.IPNets(),
 		})
 	}
 
-	return wg.ConfigureDevice(WG_DEVICENAME, updates)
+	return updates, nil
 }
 
 func main() {
+	simulate := false
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "simulate":
+			simulate = true
+		default:
+			log.Fatalln("unknown arguments. Currently only 'simulate' is supported")
+		}
+	}
+
 	etcd, err := ffbs.CreateEtcdConnection()
 	if err != nil {
 		log.Fatalln("Couldn't setup etcd connection:", err)
@@ -103,8 +150,27 @@ func main() {
 	}
 
 	for {
-		if err := update(etcd, wg); err != nil {
-			log.Println("Error trying to update the nodes:", err)
+		// misusing a loop to break at any moment and still run the sleep call
+		for {
+			updates, err := calculateWGPeerUpdates(etcd, wg)
+			if err != nil {
+				log.Println("Error trying to determine the node updates:", err)
+				break
+			}
+			if simulate {
+				fmt.Printf("Peer updates: %v\n", updates)
+				return
+			}
+			if len(updates) == 0 {
+				break
+			}
+
+			if err := wg.ConfigureDevice(WG_DEVICENAME, wgtypes.Config{Peers: updates}); err != nil {
+				log.Println("Error trying to apply the node updates:", err)
+				break
+			}
+			log.Println("Updated", len(updates), "peers")
+			break
 		}
 		time.Sleep(60 * time.Second)
 	}
--- ffbs/structs.go
+++ ffbs/structs.go
@@ -1,5 +1,10 @@
 package ffbs
 
+import (
+	"net"
+	"time"
+)
+
 type ConcentratorInfo struct {
 	Address4 string `json:"address4"`
 	Address6 string `json:"address6"`
@@ -19,4 +24,27 @@ type NodeInfo struct {
 	Range6            *string            `json:"range6,omitempty" etcd:"range6"`
 	Address4          *string            `json:"address4,omitempty" etcd:"address4"`
 	Address6          *string            `json:"address6,omitempty" etcd:"address6"`
+}
+
+func (ni NodeInfo) IPNets() []net.IPNet {
+	nets := make([]net.IPNet, 0, 2)
+	_, range4, err := net.ParseCIDR(*ni.Range4)
+	if err == nil && range4 != nil {
+		nets = append(nets, *range4)
+	}
+	_, range6, err := net.ParseCIDR(*ni.Range6)
+	if err == nil && range6 != nil {
+		nets = append(nets, *range6)
+	}
+
+	return nets
+}
+
+func (ni NodeInfo) WGKeepaliveTime() *time.Duration {
+	if ni.WGKeepalive == nil {
+		return nil
+	}
+
+	res := time.Duration(*ni.WGKeepalive) * time.Second
+	return &res
 }
